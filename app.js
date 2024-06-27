36f2139558af5f1609632e91c48da29728470cff
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-28 01:38:01 +0300

    add get update genres

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+//add middleware to parse the json sent in the request:
+app.use(express.json());
+
 //get all genres
 app.get('/api/genres', (req, res) => {
   res.status(200).json({ data: { genres: '<list of all genres>' } });
 });
+
 //create new genres
 app.post('/api/genres', (req, res) => {
   const newGenres = req.body;
 
   res.status(201).json({ data: { genres: '<genres created>' } });
 });
+
 // get single genres
 app.get('/api/genres/:id', (req, res) => {
   // get id from the request parameter
   const id = req.params.id;
   //Search for id...
-  res.status(201).json({ data: { genres: '<single genres>' } });
+  res.status(200).json({ data: { genres: '<single genres>' } });
+});
+
+//Update genres
+app.put('/api/genres/:id', (req, res) => {
+  // get id from the request parameter
+  const id = req.params.id;
+  res.status(200).json({ data: { genres: '<Updated genres>' } });
 });
 
 //run the server and wait for requests
