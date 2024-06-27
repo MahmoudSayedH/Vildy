6fce693b3bb7fcfc2f78de5552efdc64c988e046
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-28 01:23:50 +0300

    create get all genres route

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const morgan = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
-
+//get all genres
+app.get('/api/genres', (req, res) => {
+  res.status(200).json({ data: { genres: '<list of all genres>' } });
+});
 //run the server and wait for requests
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port: ${port} ...`));
