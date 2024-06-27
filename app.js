7599208ab51f33794d2e5dfad250329df3744a23
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-28 01:36:12 +0300

    add get single genres

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ app.get('/api/genres', (req, res) => {
 app.post('/api/genres', (req, res) => {
   const newGenres = req.body;
 
-  res.status(200).json({ data: { genres: '<genres created>' } });
+  res.status(201).json({ data: { genres: '<genres created>' } });
 });
+// get single genres
+app.get('/api/genres/:id', (req, res) => {
+  // get id from the request parameter
+  const id = req.params.id;
+  //Search for id...
+  res.status(201).json({ data: { genres: '<single genres>' } });
+});
+
 //run the server and wait for requests
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is running on port: ${port} ...`));
