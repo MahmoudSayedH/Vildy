92f423ed91aa72d8feb6a3125f4e1e746ecf0262
Mahmoud Sayed Hassan <mahmoudsayed671@gmail.com>
2024-06-30 01:03:33 +0300

    connect to mongodb: add controllers

--- controllers/genresController.js
+++ controllers/genresController.js
@@ -1,25 +1,45 @@
-const getAllGenres = (req, res) => {
-  res.status(200).json({ status: 'success', data: { genres: '<list of all genres>' } });
+const Genre = require('./../models/genreModel');
+const getAllGenres = async (req, res) => {
+  //get all genres from the database
+  const genres = await Genre.find();
+  //send response contain all Genres
+  res.status(200).json({ status: 'success', data: { genres } });
 };
-const createGenre = (req, res) => {
-  const newGenres = req.body;
-
-  res.status(201).json({ status: 'success', data: { genres: '<genres created>' } });
+const createGenre = async (req, res) => {
+  //create new Genre from the reqest body
+  const newGenres = await Genre.create(req.body);
+  //send response of the new created genre
+  res.status(201).json({ status: 'success', data: { genre: newGenres } });
 };
-const getGenre = (req, res) => {
+const getGenre = async (req, res) => {
   // get id from the request parameter
   const id = req.params.id;
-  //Search for id...
-  res.status(200).json({ status: 'success', data: { genres: '<single genres>' } });
+  //Search for id in the database
+  const genre = Genre.find(id);
+  // send response contains the data of the recived id
+  res.status(200).json({ status: 'success', data: { genre } });
 };
-const updateGenre = (req, res) => {
+const updateGenre = async (req, res) => {
   // get id from the request parameter
   const id = req.params.id;
-  res.status(200).json({ status: 'success', data: { genres: '<Updated genres>' } });
+  const newData = req.body;
+  //find genre and update its value
+  const genre = await Genre.findByIdAndUpdate(
+    id,
+    newData, //we can add this mongoose code here in the second argument:($set{}-$inc{}-->)
+    {
+      new: true, //get the new updated genre
+      runValidator: true, //run validation on the new inserted data
+    }
+  );
+  //send a response of the updated genre
+  res.status(200).json({ status: 'success', data: { genre } });
 };
-const deleteGenre = (req, res) => {
+const deleteGenre = async (req, res) => {
   // get id from the request parameter
   const id = req.params.id;
-  res.status(204).json({ status: 'success', data: { genres: null } });
+  //find a data by id then delete from database
+  await Genre.findByIdAndDelete(id);
+  res.status(204).json({ status: 'success', data: { genre: null } });
 };
 module.exports = { getAllGenres, createGenre, getGenre, updateGenre, deleteGenre };
